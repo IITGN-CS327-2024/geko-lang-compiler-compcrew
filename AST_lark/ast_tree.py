c3349ceccf13b0a0652c54dafd0c93690eb965c2
DarshiDoshi <102381142+DarshiDoshi@users.noreply.github.com>
2024-04-20 19:13:07 +0530

    updated GivenOtherOtherwise

--- AST_lark/ast_tree.py
+++ AST_lark/ast_tree.py
@@ -16,12 +16,16 @@ def build_tree(node):
         for stmt in node.statements[:-1]:
             main_func_node.children.append(build_tree(stmt))
         return main_func_node
-        
-    # Darshi is writing this
-    # elif isinstance(node, ShowStatement):
-    #     show_node = TreeNode("Show")
-    #     for expr in node.expressions:
-
+    elif isinstance(node, ShowStatement):
+        show_node = TreeNode("Show")
+        for expr in node.expressions:
+            show_node.children.append(build_tree(expr))
+        return show_node
+    elif isinstance(node, EnterStatement):
+        enter_node = TreeNode("Enter")
+        enter_node.children.append(TreeNode(node.string))
+        enter_node.children.append(TreeNode(node.enter_value))
+        return enter_node
     elif isinstance(node, Array):
         array_node = TreeNode("Array")
         array_node.children.append(build_tree(node.size))
@@ -32,23 +36,26 @@ def build_tree(node):
         var_decl_node.children.append(TreeNode(f"{node.variable_name}"))
         if node.size_array is not None:
             var_decl_node.children.append(TreeNode(f"{node.size_array}"))
-        print(len(node.equal_to))
         if node.equal_to:
-            if (len(node.equal_to) == 3):
-                for elements in node.equal_to:
-                    if elements is not None:
-                        equal_to_subtree = build_tree(elements)
-                        if equal_to_subtree is not None:
-                            var_decl_node.children.append(equal_to_subtree)
+            if (type(node.equal_to) is list):
+                if (node.equal_to[0].__class__.__name__ == 'EnterStatement'):
+                    var_decl_node.children.append(build_tree(node.equal_to[0]))
+                elif (len(node.equal_to) == 3):
+                    for elements in node.equal_to:
+                        if elements is not None:
+                            equal_to_subtree = build_tree(elements)
+                            if equal_to_subtree is not None:
+                                var_decl_node.children.append(equal_to_subtree)
+                else:
+                    expr = TreeNode("Equation")
+                    var_decl_node.children.append(expr)
+                    for elements in node.equal_to:
+                        if elements is not None:
+                            equal_to_subtree = build_tree(elements)
+                            if equal_to_subtree is not None:
+                                expr.children.append(TreeNode(equal_to_subtree))
             else:
-                expr = TreeNode("Equation")
-                var_decl_node.children.append(expr)
-                for elements in node.equal_to:
-                    if elements is not None:
-                        equal_to_subtree = build_tree(elements)
-                        print("equal_to_subtree mai hai hum ab")
-                        if equal_to_subtree is not None:
-                            expr.children.append(TreeNode(equal_to_subtree))
+                var_decl_node.children.append(build_tree(node.equal_to))
         # if node.initial_value:
         #     var_decl_node.children.append(build_tree(node.initial_value))
         # if node.equal_to:
@@ -62,7 +69,7 @@ def build_tree(node):
         return assignment_node
     elif isinstance(node, Expression):
         # expr_node = TreeNode("Expression")
-        op_node = TreeNode("NoOp")
+        # op_node = TreeNode("NoOp")
         if node.operator_if_exists:
             print("operator_if_exists mai hai hum ab")
             # expr_node.children.append(TreeNode(f"Operator: {node.operator_if_exists}"))
@@ -72,28 +79,34 @@ def build_tree(node):
                 op_node.children.append(build_tree(term))
             return op_node
         else:
-            print("operator_if_exists mai nahi hai hum ab")
+            # print("operator_if_exists mai nahi hai hum ab")
             for term in node.terms:
-                op_node.children.append(build_tree(term))
-            return op_node
+                return build_tree(term)
         #     expr_node.children.append(build_tree(term))
     elif isinstance(node, Term):
-        # term_node = TreeNode("Term")
-        # if node.value:
-        #     term_node.children.append(TreeNode(f"value: {node.value}"))
-        # if node.identifier:
-        #     term_node.children.append(TreeNode(f"identifier: {node.identifier}"))
-        # if node.expression:
-        #     term_node.children.append(build_tree(node.expression))
-        # if node.unary_operator:
-        #     term_node.children.append(TreeNode(f"unary_operator: {node.unary_operator}"))
-        # return term_node
         if node.value:
             return node.value
         elif node.identifier:
             return node.identifier
         # elif node.expression:
         #     return build_tree(node.expression)
+    elif isinstance(node, ConditionalStatement):
+        cond_node = TreeNode("Conditions")
+        cond_node.children.append(build_tree(node.conditional_argument))
+        cond_node.children.append(build_tree(node.conditional_block))
+        for other in node.other_blocks:
+            cond_node.children.append(build_tree(other))
+        cond_node.children.append(build_tree(node.otherwise_block))
+        return cond_node
+    elif isinstance(node, ConditionalArgument):
+        inside_given_node = TreeNode("GivenArg")
+        if node.is_special is not None:
+            print("CompCrew")
+        if node.comparison_operator is not None:
+            print("CompCrew1")
+        if node.expression is not None:
+            inside_given_node.children.append(build_tree(node.expression))
+        return inside_given_node
     elif isinstance(node, BinaryOperator):
         # bin_op_node = TreeNode("BinaryOperator")
         # bin_op_node.children.append(TreeNode(f"operator: {node.operator}"))
